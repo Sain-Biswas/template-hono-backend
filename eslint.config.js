b526650ac377c948995ce00a05ebf059603321f1
Sain-Biswas <sain.biswas614@gmail.com>
2025-03-21 14:51:16 +0530

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,16 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
+  },
+
+  {
+    plugins: { "@stylistic": stylistic },
   },
 
   {
@@ -21,6 +26,24 @@ export default [
   {
     rules: {
       "no-duplicate-imports": ["error"],
+      "no-template-curly-in-string": ["warn"],
+      camelcase: ["error"],
+      curly: ["error", "multi"],
+      "prefer-arrow-callback" : ["warn"]
+    },
+  },
+
+  {
+    rules: {
+      "@stylistic/curly-newline": ["warn", "always"],
+      "@stylistic/semi": ["warn", "always"],
+      "@stylistic/indent": ["warn", 2],
+      "@stylistic/arrow-spacing": ["warn", { before: true, after: true }],
+      "@stylistic/template-curly-spacing": ["warn", "always"],
+      "@stylistic/block-spacing": ["warn"],
+      "@stylistic/brace-style": ["warn", "1tbs"],
+      "@stylistic/comma-spacing": ["warn", { "before": false, "after": true }],
+      "@stylistic/comma-style": ["warn", "last"],
     },
   },
 ];
